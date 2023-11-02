860b02fb6d928d128b3479d0719737be4cc7b080
zhiwei <zhr14@pitt.edu>
2023-11-02 16:51:41 -0400

    Acceptance rate revert

---
+++ common/build-info.cpp
@@ -0,0 +1,4 @@
+int LLAMA_BUILD_NUMBER = 1477;
+char const *LLAMA_COMMIT = "5f5e264";
+char const *LLAMA_COMPILER = "clang version 16.0.6";
+char const *LLAMA_BUILD_TARGET = "aarch64-unknown-linux-android24";
